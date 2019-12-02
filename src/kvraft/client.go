376588bfff6b5eb1bae26c0c1c68866cc5ddd940
chinawch007 <chinawch_007@163.com>
2019-12-02 17:56:18 +0800

    pass manority 3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,9 +1,29 @@
 package raftkv
 
-import "labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"time"
 
+	"../labrpc"
+)
+
+/*
+	多个机器同时发请求,处理好并发---看看怎么加锁---话说raft中真的不需要加什么锁吗?
+	clerk发到错的机器,重试---follower机器该不该知道leader是谁?
+	用回复rpc的方式回复clerk成功了,就是说kvserver得管理管道了是吧
+	leader换人了,想想就头疼---需要重试
+	写入raft日志的结构是op
+	kvserver同raft的交流,教程中说用管道?
+	start的管道,和别人的请求同时处理,并发问题,别弄出死锁了
+	通过同一个index的日志是否出现来确定当前,任期是否变化---第二个hint说可以无限等
+	缓存leader
+	不在大多数中不该处理get请求?话说我咋知道我在不在大多数中?
+	返回失败,但其实成功的情况,如何保持幂等
+	超时重试,又发给另一个任期的leader
+
+*/
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
@@ -36,10 +56,81 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 //
+//让follower告知我谁是leader还是我自己去轮询,考虑下
+//因为可以无限重试,此调用一定会返回一个串
+//无限循环,必保成功
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+	var args GetArgs
+	args.Key = key
+	args.Ts = time.Now().UnixNano() / 1000000
+
+	var value string
+
+	fmt.Printf("kvclient get start, key: %v\n", key)
+	successed := false
+RELOOP:
+	//一直失败,超级大循环
+	for i := 0; i < len(ck.servers); i++ {
+		var reply GetReply
+		ok := ck.servers[i].Call("KVServer.Get", &args, &reply)
+		if ok {
+			if !reply.WrongLeader {
+				if reply.Err == OK {
+					fmt.Printf("kvclient get to %d successed, key: %v, value: %v\n", i, key, reply.Value)
+					value = reply.Value
+					return value
+				} else if reply.Err == KeyNotExist {
+					fmt.Printf("kvclient get to %d, key not exist, key: %v, value: %v\n", i, key, reply.Value)
+					return ""
+				} else if reply.Err == ErrTimeout {
+					fmt.Printf("kvclient get to %d timeout, key: %v, value: %v\n", i, key, reply.Value)
+					//value = reply.Value
+					//return reply.Value
+					continue
+				} else { //暂时没有这种错误
+					//如果他是leader的话,我对他重试是没问题的
+					//---这中情况下应该是当机了,我还是重新开始在来一轮吧,反正无限等待
+					fmt.Printf("kvclient get to %d reply error: %v ,key:%v\n", i, reply.Err, key)
+					//goto RELOOP
+					break
+				}
+			} else {
+				//不是leader继续找
+				fmt.Printf("kvclient get to %d wrong leader, key:%v\n", i, key)
+				continue
+			}
+		} else {
+			//失败了要不要重试呢
+			//---就是说我某一台机器调用失败了,要怎么重试?---本机重试?---重新循环?---接着跑?
+			//---重新循环是合理的,这机器多半断连了
+			//goto RELOOP
+			//后来想接着跑挺合理
+
+			//rpc失败要重试,以防有值但是没拿到
+			fmt.Printf("kvclient get rpc to %d failed , retry, key:%v\n", i, key)
+			continue
+			//goto RELOOP
+		}
+	}
+
+	if !successed {
+		//args.Retry = true
+		goto RELOOP
+	}
+
+	//循环结束没得到值,要重试
+	/*
+		if reply.Value == "" {
+			fmt.Printf("kvclient get,after a loop not get the value, loop again, key:%v\n", key)
+			goto RELOOP
+		}
+	*/
+	//你可得保证没得到值时这个返回是空串
+	//fmt.Printf("kvclient get over\n")
+
+	return value
 }
 
 //
@@ -52,8 +143,93 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 //
+//你这个是客户端库函数,没有返回成功失败吗...
+//问题在于,超时情况下,会在哪个错误条件下返回?
+//无限循环,必保成功
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	var args PutAppendArgs
+
+	args.Key = key
+	args.Value = value
+	args.Op = op
+	args.Retry = false
+	args.Ts = time.Now().UnixNano() / 1000000
+
+	fmt.Printf("kvclient put start, key: %v, value:%v, op:%v\n", key, value, op)
+
+	//按说我可以无限循环,必保成功,反正raft理论上会恢复
+	success := false
+	needRetry := false
+RELOOP:
+
+	//一直失败,超级大循环
+	//---包括leader正确返回错误重试,要有限度?
+	for i := 0; i < len(ck.servers); i++ {
+
+		/*
+			if mayBeChangeLeader { //要get一下,如果上次调用超时但实际set成功了,直接返回
+				retGet := ck.Get(key)
+				if retGet == value {
+					return
+				}
+			}
+		*/
+		var reply PutAppendReply
+		ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
+
+		if ok {
+			if !reply.WrongLeader {
+				if reply.Err == OK {
+					fmt.Printf("kvclient put to %d successed, key: %v, value:%v, op:%v\n", i, key, value, op)
+					success = true
+					needRetry = false
+					break
+				} else if reply.Err == AlreadySuccessed {
+					fmt.Printf("kvclient put to %d already successed, key: %v, value:%v, op:%v\n", i, key, value, op)
+					success = true
+					needRetry = false
+					break
+				} else if reply.Err == ErrTimeout {
+					fmt.Printf("kvclient put to %d timeout, key: %v, value:%v, op:%v\n", i, key, value, op)
+					//success = true
+					//needRetry = false
+					continue
+				} else { //暂时没有其他错误
+					//如果他是leader的话,我对他重试是没问题的
+					fmt.Printf("kvclient put to %d failed, error:%v,key: %v, value:%v, op:%v\n", i, reply.Err, key, value, op)
+					goto RELOOP
+				}
+			} else {
+				fmt.Printf("kvclient put to %d wrong leader, key: %v, value:%v, op:%v\n", i, key, value, op)
+				continue
+			}
+		} else { //这种情况下,有可能执行成功但超时了---
+			//mayBeChangeLeader = true
+			fmt.Printf("kvclient put to %d rpc error, retry, key: %v, value:%v, op:%v\n", i, key, value, op)
+			//那我怎么检查之前成没成功啊
+			//---server端想办法检查
+			needRetry = true
+			//args.RetryIndex =
+			//goto RELOOP
+			continue
+		}
+	}
+
+	if needRetry {
+		args.Retry = true //放在上边就错了
+		goto RELOOP
+	}
+
+	//循环结束没有成功,要重试
+	//---要不要睡一会,等等看
+	if !success {
+		fmt.Printf("kvclient put after a loop not successed, need loop again,sleep awhile key: %v, value:%v, op:%v\n", key, value, op)
+		time.Sleep(time.Duration(1000000000))
+		fmt.Printf("kvclient put after a loop not successed, wake up,key: %v, value:%v, op:%v\n", key, value, op)
+		goto RELOOP
+	}
+
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -1,12 +1,34 @@
 package raftkv
 
 import (
-	"labgob"
-	"labrpc"
+	"bytes"
+	"fmt"
 	"log"
-	"raft"
+	"reflect"
+	"time"
+
+	"../raft"
+
 	"sync"
+
+	"../labgob"
+	"../labrpc"
 )
+
+/*
+	切片层按说是kvserver的功能吧,毕竟是他要读切片恢复状态
+	不能再有对要回收的内存的引用,go本身的垃圾回收
+	tester会传一个界限值过来,当日志含量接近界限值时切片
+	persister.SaveStateAndSnapshot()---这东西该是谁负责呢?kvserver倒也可以用这个类
+	raft主发起切片给follower,然后再传给kvserver?---有点理解了,raft当然只是日志的管理员,解释全在kvserver手里,切片应该是一个map序列化后的字节序列
+	先从raft那里拿到字节序列,我执行生成一个table,在序列化成切片,这个切片,理应有我来保存---如果persister能存状态和切片的话,有raft保存也可以
+	测试的时候把我搞当机用的是什么方式?
+
+	怎么抛弃旧日志是个问题,旧日志不能再有引用,用go的垃圾回收?
+	快照除了存成map还有其他方式吗?
+	很致命的一点,主截断之后,要让follower也截断,append->follower->pipe->kvserver---这个触发的工作交给raft而不是kv也有合理性
+	---我为什么要在rpc中传快照信息,话说我就让follower自己截断日志不行吗?
+*/
 
 const Debug = 0
 
@@ -17,11 +39,14 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Key   string
+	Value string
+	Op    string
+	Ts    int64
 }
 
 type KVServer struct {
@@ -33,15 +58,278 @@ type KVServer struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	Table map[string]string
+
+	LastCommitIndex int
+	//LastCommitTerm  int
+
+	WaitCommitInterval int
+
+	//看有没有必要就复用raft的那个persister---牛马不相及复用啥
+	SnapshotPersister *raft.Persister
 }
 
+func (kv *KVServer) isLeader() bool {
+	_, isLeader := kv.rf.GetState()
 
+	return isLeader
+}
+
+//我不能给客户端返回,客户端那边什么症状?
+//---如果调到小部分的leader,会无限阻塞
+//---所以恐怕还是要加超时机制
+//---想办法辞职吧...
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	//这个锁跟raft的锁没关系,你加了raft也可能不是leader了,只能get完之后检查下
+	fmt.Printf("kvserver:%d get start key:%v\n", kv.me, args.Key)
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	if !kv.isLeader() {
+		reply.WrongLeader = true
+		fmt.Printf("kvserver:%d get not leader, return, key:%v\n", kv.me, args.Key)
+		return
+	}
+
+	reply.WrongLeader = false
+	val, isExist := kv.Table[args.Key]
+	if isExist {
+		reply.Value = val
+		reply.Err = OK
+		fmt.Printf("kvserver:%d get successed, key:%v, value:%v\n", kv.me, args.Key, val)
+	} else {
+		reply.Err = KeyNotExist
+		fmt.Printf("kvserver:%d get not exist, key:%v\n", kv.me, args.Key)
+		//不能返回,你找不到新主或许能找到
+	}
+
+	//此部分从putAppend搬来,验证连通性,证明主依然在多数机中
+	var op Op
+	op.Op = "Get"
+	op.Key = args.Key
+	op.Value = "_"
+
+	kv.rf.Start(op)
+	//fmt.Printf("kvserver:%d after raft start,wait for the chan msg, key:%v,value:%v\n", kv.me, args.Key, args.Value)
+
+	for {
+		var m raft.ApplyMsg
+		select {
+		case m = <-kv.applyCh:
+			{
+				fmt.Printf("kvserver:%d get the msg from chan, key:%v\n", kv.me, args.Key)
+				fmt.Printf("kvserver:%d CommandValid:%t, CommandIndex:%d,", kv.me, m.CommandValid, m.CommandIndex)
+				fmt.Println(m.Command)
+				/*
+					if m.Command.(Op) != op {
+						kv.CommitOp(m.Command.(Op)) //暂时先应用下
+						continue
+					}
+				*/
+
+				if m.CommandValid && m.CommandIndex > kv.LastCommitIndex {
+					kv.CommitOp(m.Command.(Op))
+					kv.LastCommitIndex = m.CommandIndex
+					reply.Err = OK
+					reply.WrongLeader = false
+					//return
+				} else {
+					//超时了,实际可能是提交了的,然后我这边怎么处理呢?---get一下,看看是不是设定的值
+					//reply.Err = ErrTimeout
+					//reply.WrongLeader = false
+					fmt.Printf("kvserver:%d,exception \n", kv.me)
+				}
+
+				if m.Command.(Op) == op {
+					fmt.Printf("kvserver:%d reply msg:", kv.me)
+					fmt.Println(reply)
+					return
+				}
+			}
+		case <-time.After(30 * 100 * time.Millisecond):
+			{
+				reply.Err = ErrTimeout
+				return
+			}
+		default:
+		}
+	}
+	//接收后不做行动了,只是为了验证多数连通性
+	//如果始终不返回,那要一直阻塞,这锁也不释放吗...
+	//中间穿插着不是leader又变回来也没啥,是leader就是绝对权威
+	if !kv.isLeader() {
+		reply.WrongLeader = true
+		fmt.Printf("kvserver:%d get not leader after, key:%v\n", kv.me, args.Key)
+	}
 }
 
+func (kv *KVServer) CommitOp(op Op) {
+	if op.Op == "Put" {
+		kv.Table[op.Key] = op.Value
+	} else if op.Op == "Append" { //也不知道append的定义是不是这个
+		kv.Table[op.Key] += op.Value
+	}
+
+	fmt.Printf("kvserver:%d commit value into table:%v\n", kv.me, kv.Table[op.Key])
+}
+
+//raft要保证是按序提交的,即后start的op,返回过来的值要后到---这是他们讨论过的乱序提交吗?
+//看看这个routine是持久存在的好还是每个请求一个的好---一个就够了,不然多个routine还需要多个chan
+//---follower的msg如果不读出来有积存,raft就不能往里推了,所以必然得有这么个routine平时去读,并且把日志应用于Table
+//---但还要跟leader的接口有竞争,所以加锁非阻塞模式.
+
+//这个基本上只为了follower服务了,leader是不会接收消息引起这个的
+func (kv *KVServer) RecvRaftMsgRoutine() {
+	for {
+		kv.mu.Lock()
+		//fmt.Printf("kvserver:%d lock\n", kv.me)
+		var m raft.ApplyMsg
+
+		select {
+		case m = <-kv.applyCh:
+			{
+				fmt.Printf("kvserver:%d follower get msg from raft CommandValid:%t, CommandIndex:%d\n", kv.me, m.CommandValid, m.CommandIndex)
+				fmt.Println(m.Command)
+
+				//对get命令做特殊处理
+				/*
+					if m.Command.(Op).Op == "Get" {
+						fmt.Printf("kvserver:%d, Get command msg, ignored\n", kv.me)
+						continue
+					}
+				*/
+
+				//后来条件其实没啥用---会不会出现阻塞延迟,结果每一条消息都延迟一个阶段才来---看测试吧
+				//这个要保证一次来一条是吧
+				if m.CommandValid && m.CommandIndex == kv.LastCommitIndex+1 {
+					kv.CommitOp(m.Command.(Op))
+					kv.LastCommitIndex++
+				} else {
+					//超时了,实际可能是提交了的,然后我这边怎么处理呢?---get一下,看看是不是设定的值
+					fmt.Printf("kvserver:%d follower not get msg from raft\n", kv.me)
+				}
+			}
+		default:
+			{
+
+			}
+
+		}
+
+		kv.mu.Unlock()
+		//fmt.Printf("kvserver:%d unlock\n", kv.me)
+		time.Sleep(time.Duration(200 * 1000000))
+	}
+}
+
+//并发调用,你就当成是多线程调用这个函数
+//---发现了个问题,新主提交旧日志的时候,我这里接收到的不是目的值---描述下我这种1个1个提交的运行状态
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	fmt.Printf("kvserver:%d putappend start, key:%v,value:%v\n", kv.me, args.Key, args.Value)
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	if !kv.isLeader() {
+		reply.WrongLeader = true
+		fmt.Printf("kvserver:%d PutAppend not leader, return, key:%v\n", kv.me, args.Key)
+		return
+	}
+
+	reply.TargetIndex = len(kv.rf.Log)
+
+	var op Op
+	op.Op = args.Op
+	op.Key = args.Key
+	op.Value = args.Value
+	op.Ts = args.Ts //下层还是比较command,加了这个之后就能区别出其他的请求了
+
+	if args.Retry && kv.rf.SearchLog(op) {
+		//if args.Retry && kv.rf.Log[args.RetryIndex].Command.(Op) == op {
+		reply.Err = AlreadySuccessed
+		return
+	}
+
+	//并发被调,等待raft管道返回,返回的顺序不确定,不知道应答对应哪个请求,加锁顺序调用吧---raft那边其实也没啥并发度吧
+	//---其实根据管道过来的消息加个字段可以知道具体的应答client
+	//---你说的这样得你自己做事件驱动消息分发,这里边的框架就是一个函数完成后要结果
+
+	//raft立即就返回了,但client那边还在等我的回复,所以我要等raft处理完,等管道
+	kv.rf.Start(op)
+	fmt.Printf("kvserver:%d after raft start,wait for the chan msg, key:%v,value:%v\n", kv.me, args.Key, args.Value)
+
+	for {
+		//第一次通道会阻塞,但我要控制超时---这个第一次是啥意思来着?---当时为啥搞成超时模式的呢?看起来很聪明
+		//这是个问题,我要搞成无限阻塞等待的还是非阻塞立即返回的???
+		//time.Sleep(time.Duration(kv.WaitCommitInterval * 1000000))
+		var m raft.ApplyMsg
+
+		//此处是否阻塞等要讨论
+		//---如果不等的话,一会他来了,我得到下一个请求才能接收到这次的信息,可下一次请求又会带新的数据...
+		//------看能不能返回个返回码明确这种情况,下次来只是等值,不带参数---那你还不如就这么等着...
+		//---一直等,我这次的请求,可能在图8的情况下丢失...
+		//------得根据具体测试结果看,难...暂时先一直等吧
+
+		select {
+		case m = <-kv.applyCh:
+			{
+				fmt.Printf("kvserver:%d get the msg from chan, key:%v,value:%v\n", kv.me, args.Key, args.Value)
+				fmt.Printf("kvserver:%d CommandValid:%t, CommandIndex:%d,", kv.me, m.CommandValid, m.CommandIndex)
+				fmt.Println(m.Command)
+
+				//第一次接收可能会接收多条,我要一直等直到收到这次这条
+				/*
+					if m.Command.(Op) != op {
+						kv.CommitOp(m.Command.(Op)) //暂时先应用下
+						continue
+					}
+				*/
+
+				//后来条件其实没啥用---会不会出现阻塞延迟,结果每一条消息都延迟一个阶段才来---看测试吧
+				//每条都提价,但返回要看情况
+				if m.CommandValid && m.CommandIndex > kv.LastCommitIndex {
+					kv.CommitOp(m.Command.(Op))
+					kv.LastCommitIndex = m.CommandIndex
+					reply.Err = OK
+					reply.WrongLeader = false
+					//return
+				} else {
+					//超时了,实际可能是提交了的,然后我这边怎么处理呢?---get一下,看看是不是设定的值
+					//reply.Err = ErrTimeout
+					//reply.WrongLeader = false
+					fmt.Printf("kvserver:%d,exception \n", kv.me)
+				}
+
+				//前边的是都做应用了,这里一直要等到我这次提的请求才能正确返回
+				if m.Command.(Op) == op {
+					fmt.Printf("kvserver:%d reply msg:", kv.me)
+					fmt.Println(reply)
+					return
+				}
+			}
+		//超时有client端重试,那边会遍历所有的server
+		case <-time.After(3 * 1000 * time.Millisecond):
+			{
+				reply.Err = ErrTimeout
+				reply.WrongLeader = false
+				return
+			}
+		default:
+		}
+
+	}
+
+	/*
+		if kv.rf.persister.RaftStateSize() > kv.maxraftstate {
+			kv.makeSnapshot()
+		}
+	*/
+
+	if !kv.isLeader() {
+		fmt.Printf("kvserver:%d PutAppend not leader after, key:%v\n", kv.me, args.Key)
+		reply.WrongLeader = true
+		return
+	}
 }
 
 //
@@ -77,13 +365,58 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv := new(KVServer)
 	kv.me = me
 	kv.maxraftstate = maxraftstate
+	kv.Table = make(map[string]string)
 
 	// You may need initialization code here.
 
-	kv.applyCh = make(chan raft.ApplyMsg)
+	//这边要搞成一个有缓冲的chan,这样raft那边推就不会阻塞了,
+	kv.applyCh = make(chan raft.ApplyMsg, 100)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	kv.LastCommitIndex = 0
+	//kv.LastCommitTerm = -1
+
+	kv.SnapshotPersister = new(raft.Persister)
+
+	for i := 0; i < len(kv.rf.Log); i++ {
+		kv.CommitOp(kv.rf.Log[i].Command.(Op))
+	}
+
+	go kv.RecvRaftMsgRoutine()
+
+	fmt.Printf("kvserver:%dstart over\n", kv.me)
 
 	return kv
+}
+
+//前半部分生成状态且从日志中删除---后来看,为啥要部分呢?直接全部搞掉呗
+func (kv *KVServer) makeSnapshot() {
+
+	//这地方不加raft的锁没啥事,大不了跟那边差几条日志
+	index := len(kv.rf.Log) - 1
+
+	w := new(bytes.Buffer)
+	e := labgob.NewEncoder(w)
+	e.Encode(kv.rf.Log[index].Index)
+	e.Encode(kv.rf.Log[index].Term)
+
+	var table map[string]string
+	for i := 0; i <= index; i++ {
+		reflect.TypeOf(kv.rf.Log[i].Command)
+		op := kv.rf.Log[i].Command.(Op)
+
+		if op.Op == "Put" {
+			table[op.Key] = table[op.Value]
+		} else if op.Op == "Append" {
+			table[op.Key] += table[op.Value]
+		}
+	}
+	e.Encode(table)
+
+	data := w.Bytes()
+
+	kv.SnapshotPersister.SaveStateAndSnapshot(nil, data)
+
+	kv.rf.DiscardLog(index)
 }
